4229de5260d4fbafadf5756f1cf0c1a24d7462a3
Amit060 <134519626+Amit060@users.noreply.github.com>
2023-05-24 21:56:48 +0530

    File Added

---
+++ main.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+ window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+ }
+
+// let menu = document.querySelector('#menu-icon');
+//   let navbar = document.querySelector('.navbar');
+//   menu.onclick = () => {
+//     menu.classList.toggle('bx-menu');
+//     navbar.classList.toggle('active');
+//   };
